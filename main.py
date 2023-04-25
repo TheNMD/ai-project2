ec73924a00367016343b0acc2f4d1bc49ba40321
TheNMD <manhdanog@gmail.com>
2023-04-25 19:06:15 +0700

    com

--- main.py
+++ main.py
@@ -50,32 +50,40 @@ def button(): pass
 if __name__ == '__main__':
     # Pin Definitons:
     pwmPin = 18 # Broadcom pin 18 (P1 pin 12)
-    ledPin = 23 # Broadcom pin 23 (P1 pin 16)
+    # ledPin = 23 # Broadcom pin 23 (P1 pin 16)
     butPin = 17 # Broadcom pin 17 (P1 pin 11)
 
     dc = 95 # duty cycle (0-100) for PWM pin
 
     # Pin Setup:
     GPIO.setmode(GPIO.BCM) # Broadcom pin-numbering scheme
-    GPIO.setup(ledPin, GPIO.OUT) # LED pin set as output
+    # GPIO.setup(ledPin, GPIO.OUT) # LED pin set as output
     GPIO.setup(pwmPin, GPIO.OUT) # PWM pin set as output
     pwm = GPIO.PWM(pwmPin, 50)  # Initialize PWM on pwmPin 100Hz frequency
     GPIO.setup(butPin, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Button pin set as input w/ pull-up
-    GPIO.output(ledPin, GPIO.LOW)
+    
+    # GPIO.output(ledPin, GPIO.LOW)
     pwm.start(dc)
-    print("Here we go! Press CTRL+C to exit")
+    print("Press CTRL+C to exit")
+    
     try:
         while True:
             if GPIO.input(butPin): # button is released
-                pwm.ChangeDutyCycle(dc)
-                GPIO.output(ledPin, GPIO.LOW)
+                print("0")
+                time.sleep(1.0)
+                # pwm.ChangeDutyCycle(dc)
+                # GPIO.output(ledPin, GPIO.LOW)
             else: # button is pressed:
-                pwm.ChangeDutyCycle(100-dc)
-                GPIO.output(ledPin, GPIO.HIGH)
-                time.sleep(0.075)
-                GPIO.output(ledPin, GPIO.LOW)
-                time.sleep(0.075)
-    except KeyboardInterrupt: # If CTRL+C is pressed, exit cleanly:
+                print("1")
+                time.sleep(1.0)
+                # pwm.ChangeDutyCycle(100-dc)
+                # GPIO.output(ledPin, GPIO.HIGH)
+                # time.sleep(0.075)
+                # GPIO.output(ledPin, GPIO.LOW)
+                # time.sleep(0.075)
+    # If CTRL+C is pressed, exit cleanly:
+    except KeyboardInterrupt:
+        print("-1") 
         pwm.stop() # stop PWM
         GPIO.cleanup() # cleanup all GPIO
     
