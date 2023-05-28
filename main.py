c2b7dc4c2cdfcbedfeedd1986728ba1f1eb943c9
TheNMD <manhdanog@gmail.com>
2023-05-28 12:08:35 +0700

    com

--- main.py
+++ main.py
@@ -133,11 +133,11 @@ if __name__ == '__main__':
                     counter += 1
             camera = PiCamera()
             camera.start_preview()
-            time.sleep(2)
             print("Ready to take picture.\n")
             pygame.mixer.music.load(f'./audio/default/ready.mp3')
             pygame.mixer.music.set_volume(0.5)
             pygame.mixer.music.play()
+            # time.sleep(1)
             while True:
                 if GPIO.input(camPin) == False:
                     break
